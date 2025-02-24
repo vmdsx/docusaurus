73289f22c84917eeaa438d7bb12a4291a322810c
vmdsx <santos.victor@telefonica.com>
2025-02-24 17:39:39 -0300

    corigida a a config de url r baseURL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://vmdsx.github.io/docusaurus',
+  url: 'https://vmdsx.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
